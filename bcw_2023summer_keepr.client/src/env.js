d0186fd378dc6aeaaad4fc1972c63acb33ab3662
Arrington Mata <mataarrington@gmail.com>
2023-08-26 16:57:23 -0600

    🛠 Fixed CORS error

--- bcw_2023summer_keepr.client/src/env.js
+++ bcw_2023summer_keepr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:7045' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-p7vx5zxzsmz52nqh.us.auth0.com'
 export const clientId = 'mL5Vkm043yVJ4zUIDW65Mk9ovb8WQB4G'
