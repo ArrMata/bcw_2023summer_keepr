2d562997c6e26edf01716928166abd6a5a785f5e
Arrington Mata <mataarrington@gmail.com>
2023-08-25 19:31:02 -0600

    ✅ Account Tests pass

--- bcw_2023summer_keepr/Controllers/AccountController.cs
+++ bcw_2023summer_keepr/Controllers/AccountController.cs
@@ -6,14 +6,16 @@ public class AccountController : ControllerBase
 {
   private readonly AccountService _accountService;
   private readonly Auth0Provider _auth0Provider;
+  private readonly VaultsService _vaultsService;
 
-  public AccountController(AccountService accountService, Auth0Provider auth0Provider)
-  {
-    _accountService = accountService;
-    _auth0Provider = auth0Provider;
-  }
+    public AccountController(AccountService accountService, Auth0Provider auth0Provider, VaultsService vaultsService)
+    {
+        _accountService = accountService;
+        _auth0Provider = auth0Provider;
+        _vaultsService = vaultsService;
+    }
 
-  [HttpGet]
+    [HttpGet]
   [Authorize]
   public async Task<ActionResult<Account>> Get()
   {
@@ -21,6 +23,22 @@ public class AccountController : ControllerBase
     {
       Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
       return Ok(_accountService.GetOrCreateProfile(userInfo));
+    }
+    catch (Exception e)
+    {
+      return BadRequest(e.Message);
+    }
+  }
+
+  [HttpGet("vaults")]
+  [Authorize]
+  public async Task<ActionResult<List<Vault>>> GetAccountVaults()
+  {
+    try
+    {
+      Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      List<Vault> vaults = _vaultsService.GetVaultsByProfileId(userInfo.Id, userInfo.Id);
+      return Ok(vaults);
     }
     catch (Exception e)
     {
