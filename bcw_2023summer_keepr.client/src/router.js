4f5def26457a163afb7d4fd207ad79f68d081c36
Arrington Mata <mataarrington@gmailcom>
2023-08-28 16:44:21 -0600

    Added profile and vault routes

--- bcw_2023summer_keepr.client/src/router.js
+++ bcw_2023summer_keepr.client/src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import { authGuard } from '@bcwdev/auth0provider-client'
+import { authGuard, authSettled } from '@bcwdev/auth0provider-client'
 
 function loadPage(page) {
   return () => import(`./pages/${page}.vue`)
@@ -12,12 +12,18 @@ const routes = [
     component: loadPage('HomePage')
   },
   {
-    path: '/account',
-    name: 'Account',
-    component: loadPage('AccountPage'),
-    beforeEnter: authGuard
+    path: '/profile/:userId',
+    name: 'Profile',
+    component: loadPage('ProfilePage'),
+    beforeEnter: authSettled,
+  },
+  {
+    path: '/vault/:vaultId',
+    name: 'Vault',
+    component: loadPage('VaultPage'),
+    beforeEnter: authSettled,
   }
-]
+];
 
 export const router = createRouter({
   linkActiveClass: 'router-link-active',
