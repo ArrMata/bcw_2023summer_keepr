8eacd98dea310dff2a777ff7e1b8b1746d50158e
Arrington Mata <mataarrington@gmailcom>
2023-08-28 16:44:12 -0600

    Added appstate items

--- bcw_2023summer_keepr.client/src/AppState.js
+++ bcw_2023summer_keepr.client/src/AppState.js
@@ -5,8 +5,16 @@ export const AppState = reactive({
   user: {},
   /** @type {import('./models/Account.js').Account} */
   account: {},
+  /** @type {import('./models/Account.js').Account} */
+  activeAccount: {},
   /** @type {import('./models/Keep.js').Keep[]} */
   keeps: [],
   /** @type {import('./models/Keep.js').Keep | null} */
   activeKeep: null,
+  /** @type {import('./models/Vault.js').Vault[]} */
+  vaults: [],
+  /** @type {import('./models/Vault.js').Vault | null} */
+  activeVault: {},
+  /** @type {import('./models/Vault.js').Vault[]} */
+  accountVaults: [],
 });
